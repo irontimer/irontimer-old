c4e1f083bd64f5c211ca49be3119f13c78d6222b
Evan <evanrayweigel@outlook.com>
2022-05-25 23:57:00 -0500

    Create user.ts

---
+++ src/backend/models/user.ts
@@ -0,0 +1,69 @@
+import { User as IUser } from "../../types";
+import { Schema, model } from "mongoose";
+import { SCRAMBLE_TYPES } from "../../constants/scramble-type";
+
+export const UserSchema = new Schema<IUser>({
+  // since the userID is unique to each use we can use it as the _id
+  _id: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  discordUserID: {
+    type: String,
+    required: false
+  },
+  personalBests: {
+    type: [
+      {
+        time: {
+          type: Number,
+          required: true
+        },
+        timestamp: {
+          type: Number,
+          required: true
+        },
+        scramble: {
+          type: String,
+          required: true
+        },
+        scrambleType: {
+          type: String,
+          enum: SCRAMBLE_TYPES,
+          required: true
+        },
+        solution: {
+          type: String,
+          required: false
+        }
+      }
+    ],
+    required: true
+  },
+  canManageApiKeys: {
+    type: Boolean,
+    required: true
+  },
+  timeCubing: {
+    type: Number,
+    required: true
+  },
+  resultCount: {
+    type: Number,
+    required: true
+  },
+  lastNameChange: {
+    type: Number,
+    required: false
+  }
+});
+
+export const User = model<IUser>("user", UserSchema);
