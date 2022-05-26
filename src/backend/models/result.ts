474710b938ddd20872e58ee93eb997417fb7ceb2
Evan <evanrayweigel@outlook.com>
2022-05-25 23:56:24 -0500

    Create result.ts

---
+++ src/backend/models/result.ts
@@ -0,0 +1,33 @@
+import { SavedResult as IResult } from "../../types";
+import { Schema, model } from "mongoose";
+
+export const ResultSchema = new Schema<IResult>({
+  _id: Schema.Types.ObjectId,
+  userID: {
+    type: String,
+    required: true
+  },
+  time: {
+    type: Number,
+    required: true
+  },
+  timestamp: {
+    type: Number,
+    required: true
+  },
+  scrambleType: {
+    type: String,
+    enum: ["3x3x3", "2x2x2"],
+    required: true
+  },
+  scramble: {
+    type: String,
+    required: true
+  },
+  solution: {
+    type: [String],
+    required: false
+  }
+});
+
+export const Result = model<IResult>("result", ResultSchema);
