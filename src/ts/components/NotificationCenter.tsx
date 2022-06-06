b785d7e8099939c868dc3d6f1dd8444f9c8d7c9a
Evan <evanrayweigel@outlook.com>
2022-06-06 17:18:27 -0500

    hiding the notifications when solving

--- src/ts/components/NotificationCenter.tsx
+++ src/ts/components/NotificationCenter.tsx
@@ -1,25 +1,28 @@
-import { Component, For } from "solid-js";
+import { Component, For, Show } from "solid-js";
 import Notifications, { notificationBuffer } from "../state/notifications";
+import { isTiming } from "./TimerStopwatch";
 
 export const NotificationCenter: Component = () => {
   return (
     <div id="notification-center">
-      <For each={Object.keys(notificationBuffer).reverse()}>
-        {(id) => {
-          const notification = notificationBuffer[id];
+      <Show when={!isTiming()}>
+        <For each={Object.keys(notificationBuffer).reverse()}>
+          {(id) => {
+            const notification = notificationBuffer[id];
 
-          return (
-            <div
-              class={`notification ${notification.type}`}
-              onClick={() => Notifications.delete(id)}
-            >
-              <div class="notification-message unselectable">
-                {notification.message}
+            return (
+              <div
+                class={`notification ${notification.type}`}
+                onClick={() => Notifications.delete(id)}
+              >
+                <div class="notification-message unselectable">
+                  {notification.message}
+                </div>
               </div>
-            </div>
-          );
-        }}
-      </For>
+            );
+          }}
+        </For>
+      </Show>
     </div>
   );
 };
