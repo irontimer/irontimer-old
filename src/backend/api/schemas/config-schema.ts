798e8dedaea804b8f1ec544bc0c7da430190ff30
Evan <evanrayweigel@outlook.com>
2022-06-20 15:41:13 -0500

    this should have been number

--- src/backend/api/schemas/config-schema.ts
+++ src/backend/api/schemas/config-schema.ts
@@ -3,7 +3,7 @@ import joi from "joi";
 const CONFIG_SCHEMA = joi.object({
   timerType: joi.string().valid("timer", "typing", "stackmat").required(),
   currentSession: joi.string().required(),
-  displayAverages: joi.array().items(joi.string()).required()
+  displayAverages: joi.array().items(joi.number()).required()
 });
 
 export default CONFIG_SCHEMA;
