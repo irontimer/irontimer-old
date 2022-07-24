b121ce8528b9ca724ecf1c2f114b11e16eab1902
Ferotiq <evanrayweigel@outlook.com>
2022-07-24 00:45:12 -0500

    fix some vite config issues

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
-import { visualizer } from "rollup-plugin-visualizer";
+// import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [solidPlugin(), visualizer()],
+  plugins: [solidPlugin() /* visualizer() */],
   build: {
     target: "ESNext",
-    polyfillDynamicImport: false,
     sourcemap: true
   }
 });
